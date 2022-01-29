bbcbe59e10a8007e91fec3a0310899557256ad8c
Brad Johnson <bradsk88@gmail.com>
2022-01-29 15:43:39 -0600

    WIP: Trying to make trapar wave not block the sun
    Thanks Kaupenjoe

--- src/main/java/ca/bradj/eurekacraft/core/init/BlocksInit.java
+++ src/main/java/ca/bradj/eurekacraft/core/init/BlocksInit.java
@@ -4,9 +4,14 @@ import ca.bradj.eurekacraft.EurekaCraft;
 import ca.bradj.eurekacraft.blocks.TraparWaveBlock;
 import ca.bradj.eurekacraft.crop.FreshSeedsCrop;
 import ca.bradj.eurekacraft.materials.BrokenRefBoard;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -29,4 +34,9 @@ public class BlocksInit {
 	public static void RegisterTextures() {
 		RenderTypeLookup.setRenderLayer(FRESH_SEEDS_CROP.get(), RenderType.cutout());
 	}
+
+	public static boolean never(BlockState p_235436_0_, IBlockReader p_235436_1_, BlockPos p_235436_2_) {
+		return false;
+	}
+
 }
