fa2cbb5a5bd3d1df7bdeacf355ecf8fb3b1e5ca2
Brad Johnson <bradsk88@gmail.com>
2022-01-30 15:49:47 -0600

    Render glide board in inventory

--- src/main/java/ca/bradj/eurekacraft/core/init/ItemsInit.java
+++ src/main/java/ca/bradj/eurekacraft/core/init/ItemsInit.java
@@ -3,11 +3,13 @@ package ca.bradj.eurekacraft.core.init;
 import ca.bradj.eurekacraft.EurekaCraft;
 import ca.bradj.eurekacraft.blocks.TraparWaveBlock;
 import ca.bradj.eurekacraft.blocks.machines.RefTableBlock;
-import ca.bradj.eurekacraft.crop.FreshSeeds;
 import ca.bradj.eurekacraft.blocks.machines.ReflectionFilmScraper;
 import ca.bradj.eurekacraft.blocks.machines.ReflectionFilmScraperInit;
+import ca.bradj.eurekacraft.crop.FreshSeeds;
 import ca.bradj.eurekacraft.materials.BrokenRefBoard;
+import ca.bradj.eurekacraft.vehicles.GlideBoard;
 import ca.bradj.eurekacraft.vehicles.RefBoard;
+import ca.bradj.eurekacraft.vehicles.StandardRefBoard;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -53,9 +55,15 @@ public class ItemsInit {
 			)
 	);
 
-	public static final RegistryObject<Item> REF_BOARD = ITEMS.register(
-			RefBoard.ITEM_ID, RefBoard::new
+	public static final RegistryObject<Item> GLIDE_BOARD = ITEMS.register(
+			RefBoard.ItemIDs.GLIDE_BOARD, GlideBoard::new
 	);
+
+	public static final RegistryObject<Item> STANDARD_BOARD = ITEMS.register(
+			RefBoard.ItemIDs.REF_BOARD, StandardRefBoard::new
+	);
+
+
 
 
 }
