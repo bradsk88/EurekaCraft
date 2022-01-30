3c3813ac98e89a63dec3a4f55a0d62c61e9ec47e
Brad Johnson <bradsk88@gmail.com>
2022-01-30 13:25:54 -0600

    Attempting to mimick crafting table behaviour - abandoning to use furnace-style behaviour

--- src/main/java/ca/bradj/eurekacraft/blocks/machines/RefTableBlock.java
+++ src/main/java/ca/bradj/eurekacraft/blocks/machines/RefTableBlock.java
@@ -58,18 +58,14 @@ public class RefTableBlock extends Block {
 
     private void showUI(World world, BlockPos blockpos, PlayerEntity player) {
         if (world.isClientSide()) {
-            this.logger.debug("client side");
             return;
         }
-        this.logger.debug("client side");
 
         TileEntity te = world.getBlockEntity(blockpos);
         if (!(te instanceof RefTableTileEntity)) {
-            this.logger.debug("not RefTableTileEntity " + te);
             return;
         }
 
-        this.logger.debug("opening GUI");
         NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) te, blockpos);
     }
 }
--- src/main/java/ca/bradj/eurekacraft/blocks/machines/RefTableTileEntity.java
+++ src/main/java/ca/bradj/eurekacraft/blocks/machines/RefTableTileEntity.java
@@ -2,7 +2,6 @@ package ca.bradj.eurekacraft.blocks.machines;
 
 import ca.bradj.eurekacraft.EurekaCraft;
 import ca.bradj.eurekacraft.container.RefTableContainer;
-import ca.bradj.eurekacraft.core.init.BlocksInit;
 import ca.bradj.eurekacraft.core.init.RecipesInit;
 import ca.bradj.eurekacraft.core.init.TilesInit;
 import ca.bradj.eurekacraft.data.recipes.GlideBoardRecipe;
@@ -12,20 +11,23 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
-import net.minecraft.tileentity.LockableLootTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemStackHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,17 +35,19 @@ import org.apache.logging.log4j.Logger;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Optional;
+import java.util.Stack;
 
 public class RefTableTileEntity extends TileEntity implements INamedContainerProvider, ITickableTileEntity {
-    private final Logger logger = LogManager.getLogger(EurekaCraft.MODID);
+    private static final Logger logger = LogManager.getLogger(EurekaCraft.MODID);
 
     public static final String ENTITY_ID = "ref_table_tile_entity";
 
+    protected final Stack<ItemStack> toOutput = new Stack<>();
 
     private static int inputSlots = 6;
     private static int outputSlot = 6;
     private static int totalSlots = 7;
-    private final ItemStackHandler itemHandler = createHandler();
+    private final RefTableItemHandler itemHandler = new RefTableItemHandler(this);
     private final LazyOptional<IItemHandler> handler = LazyOptional.of(() -> itemHandler);
 
     public RefTableTileEntity(TileEntityType<?> typeIn) {
@@ -88,42 +92,86 @@ public class RefTableTileEntity extends TileEntity implements INamedContainerPro
         return super.getCapability(cap, side);
     }
 
-    private ItemStackHandler createHandler() {
-        return new ItemStackHandler(totalSlots);
-    }
-
-    public void craft() {
-        Inventory inv = new Inventory(inputSlots);
-        for (int i = 0; i < inputSlots; i++) {
-            inv.setItem(i, itemHandler.getStackInSlot(i));
-        }
-
-        Optional<GlideBoardRecipe> recipe = level.getRecipeManager().getRecipeFor(
-                RecipesInit.GLIDE_BOARD, inv, level
-        );
-
-        recipe.ifPresent(iRecipe -> {
-            logger.debug("recipe match!");
-            ItemStack output = iRecipe.getResultItem();
-
-            for (int i = 0; i < inputSlots; i++) {
-                itemHandler.extractItem(i, 1, false);
-            }
-
-            logger.debug("inserting " + output);
-            itemHandler.insertItem(outputSlot, output, false);
-
-            setChanged();
-        });
-
-    }
-
     // Crafting
     @Override
     public void tick() {
         if (level.isClientSide) {
             return;
         }
-        craft();
+        if (!this.toOutput.isEmpty()) {
+            this.toOutput.pop();
+            this.setChanged();
+        }
+    }
+
+    public static class RefTableItemHandler extends ItemStackHandler {
+
+        private final RefTableTileEntity owner;
+
+        public RefTableItemHandler(RefTableTileEntity owner) {
+            super(totalSlots);
+            this.owner = owner;
+        }
+
+
+        public void craft() {
+            World level = this.owner.getLevel();
+            if (level.isClientSide()) {
+                return;
+            }
+
+            if (!super.getStackInSlot(outputSlot).isEmpty()) {
+                return;
+            }
+
+            Inventory inv = new Inventory(inputSlots);
+            for (int i = 0; i < inputSlots; i++) {
+                inv.setItem(i, super.getStackInSlot(i));
+            }
+
+            RecipeManager recipeManager = level.getRecipeManager();
+            Optional<GlideBoardRecipe> recipe = recipeManager.getRecipeFor(
+                    RecipesInit.GLIDE_BOARD, inv, level
+            );
+
+            if (recipe.isPresent()) {
+                logger.debug("recipe match!");
+                ItemStack output = recipe.get().getResultItem();
+
+                super.insertItem(outputSlot, output, false);
+
+                logger.debug("queueing to insert" + output);
+                owner.toOutput.add(null);
+            } else {
+                ItemStack output = super.getStackInSlot(outputSlot);
+                super.extractItem(outputSlot, output.getCount(), false);
+
+                logger.debug("queueing to clear");
+                owner.toOutput.add(null);
+            }
+
+        }
+
+        @Nonnull
+        @Override
+        public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+            ItemStack output = super.insertItem(slot, stack, simulate);
+            craft();
+            return output;
+        }
+
+        @Nonnull
+        public ItemStack extractItem(int slot, int amount, boolean simulate) {
+            if (slot == outputSlot) {
+                for (int i = 0; i < inputSlots; i++) {
+                    super.extractItem(i, amount, false);
+                }
+            }
+
+            craft();
+
+            return super.extractItem(slot, amount, simulate);
+        }
+
     }
 }
