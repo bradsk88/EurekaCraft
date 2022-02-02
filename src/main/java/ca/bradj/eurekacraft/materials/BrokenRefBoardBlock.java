cf89aa3c200bbf74ebcce40ccca6e5c40467dd30
Brad Johnson <bradsk88@gmail.com>
2022-02-01 23:52:18 -0600

    Make broken ref board block direction + fix tall shack loot

--- src/main/java/ca/bradj/eurekacraft/materials/BrokenRefBoardBlock.java
+++ src/main/java/ca/bradj/eurekacraft/materials/BrokenRefBoardBlock.java
@@ -1,15 +1,16 @@
 package ca.bradj.eurekacraft.materials;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
-public class BrokenRefBoardBlock extends Block {
+import javax.annotation.Nullable;
+
+public class BrokenRefBoardBlock extends HorizontalBlock {
     public static final String ITEM_ID = "broken_ref_board_block";
     public static final Item.Properties ITEM_PROPS = new Item.Properties().
             tab(ItemGroup.TAB_MISC);
@@ -22,4 +23,16 @@ public class BrokenRefBoardBlock extends Block {
         super(PROPS);
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> b) {
+        b.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext ctx) {
+        return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+    }
+
+    // TODO: Add voxel shape to make this 2 blocks tall
 }
