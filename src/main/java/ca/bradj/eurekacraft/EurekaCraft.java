3a1ed35164ac22899dd4b68b9ecb668292dd8b90
Brad Johnson <bradsk88@gmail.com>
2022-03-09 21:06:09 -0600

    Crash server if flight is disabled (config exists to override)

--- src/main/java/ca/bradj/eurekacraft/EurekaCraft.java
+++ src/main/java/ca/bradj/eurekacraft/EurekaCraft.java
@@ -2,113 +2,92 @@ package ca.bradj.eurekacraft;
 
 import ca.bradj.eurekacraft.client.BoardItemRendering;
 import ca.bradj.eurekacraft.client.TraparStormRendering;
+import ca.bradj.eurekacraft.core.config.EurekaConfig;
 import ca.bradj.eurekacraft.core.init.*;
 import ca.bradj.eurekacraft.core.network.EurekaCraftNetwork;
 import ca.bradj.eurekacraft.render.TraparWaveHandler;
 import ca.bradj.eurekacraft.vehicles.deployment.DeploymentCapability;
 import ca.bradj.eurekacraft.world.structure.ModStructures;
-import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(EurekaCraft.MODID)
 public class EurekaCraft {
-	public static final String MODID = "eurekacraft";
+    public static final String MODID = "eurekacraft";
 
-	// Directly reference a log4j logger.
-	private static final Logger LOGGER = LogManager.getLogger();
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
 
-	public EurekaCraft() {
-		// Register the setup method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		// Register the enqueueIMC method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-		// Register the processIMC method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-		// Register the doClientStuff method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    public EurekaCraft() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-		// Register ourselves for server and other game events we are interested in
-		MinecraftForge.EVENT_BUS.register(this);
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
 
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		TilesInit.TILES.register(bus);
-		BlocksInit.BLOCKS.register(bus);
-		ItemsInit.register(bus);
-		EntitiesInit.ENTITIES.register(bus);
-		FeaturesInit.PLACEMENTS.register(bus);
-		FeaturesInit.FEATURES.register(bus);
-		ContainerTypesInit.TYPES.register(bus);
-		RecipesInit.register(bus);
-		ModStructures.STRUCTURES.register(bus);
-		AdvancementsInit.register();
-	}
+        TilesInit.TILES.register(bus);
+        BlocksInit.BLOCKS.register(bus);
+        ItemsInit.register(bus);
+        EntitiesInit.ENTITIES.register(bus);
+        FeaturesInit.PLACEMENTS.register(bus);
+        FeaturesInit.FEATURES.register(bus);
+        ContainerTypesInit.TYPES.register(bus);
+        RecipesInit.register(bus);
+        ModStructures.STRUCTURES.register(bus);
+        AdvancementsInit.register();
 
-	private void setup(final FMLCommonSetupEvent event) {
-		event.enqueueWork(ModStructures::setupStructures);
-		event.enqueueWork(DeploymentCapability::register);
-		event.enqueueWork(EurekaCraftNetwork::init);
-	}
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, EurekaConfig.SPEC, EurekaConfig.FILENAME);
+    }
 
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		// do something that can only be done on the client
-		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-		BlocksInit.RegisterTextures();
-		event.enqueueWork(ModelsInit::registerModels);
-		event.enqueueWork(() -> {
-			ClientRegistry.bindTileEntityRenderer(TilesInit.TRAPAR_WAVE.get(), TraparWaveHandler::new);
-		});
-		event.enqueueWork(BoardItemRendering::initItemProperties);
-		event.enqueueWork(TraparStormRendering::init);
-	}
+    private void setup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(ModStructures::setupStructures);
+        event.enqueueWork(DeploymentCapability::register);
+        event.enqueueWork(EurekaCraftNetwork::init);
+    }
 
-	private void enqueueIMC(final InterModEnqueueEvent event) {
-		// some example code to dispatch IMC to another mod
-		InterModComms.sendTo("eurekacraft", "helloworld", () -> {
-			LOGGER.info("Hello world from the MDK");
-			return "Hello world";
-		});
-	}
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        BlocksInit.RegisterTextures();
+        event.enqueueWork(
+                ModelsInit::registerModels
+        );
+        event.enqueueWork(
+                () -> ClientRegistry.bindTileEntityRenderer(TilesInit.TRAPAR_WAVE.get(), TraparWaveHandler::new)
+        );
+        event.enqueueWork(
+                BoardItemRendering::initItemProperties
+        );
+        event.enqueueWork(
+                TraparStormRendering::init
+        );
+    }
 
-	private void processIMC(final InterModProcessEvent event) {
-		// some example code to receive and process InterModComms from other mods
-		LOGGER.info("Got IMC {}",
-				event.getIMCStream().map(m -> m.getMessageSupplier().get()).collect(Collectors.toList()));
-	}
+    @SubscribeEvent
+    public void onServerStarting(FMLServerAboutToStartEvent event) {
+        if (EurekaConfig.crash_if_flight_disabled.get()) {
+            if (!event.getServer().isFlightAllowed()) {
+                throw new IllegalStateException(
+                        "EurekaCraft is configured to crash the server if " +
+                                "flight is disabled. You can fix this by " +
+                                "updating server.properties to enable flight, " +
+                                "or by updating world/serverconfig/" + EurekaConfig.FILENAME);
+            }
+        }
+    }
 
-	// You can use SubscribeEvent and let the Event Bus discover methods to call
-	@SubscribeEvent
-	public void onServerStarting(FMLServerStartingEvent event) {
-		// do something when the server starts
-		LOGGER.info("HELLO from server starting");
-	}
-
-	// You can use EventBusSubscriber to automatically subscribe events on the
-	// contained class (this is subscribing to the MOD
-	// Event bus for receiving Registry Events)
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-			// register a new block here
-			LOGGER.info("HELLO from Register Block");
-		}
-	}
 }
