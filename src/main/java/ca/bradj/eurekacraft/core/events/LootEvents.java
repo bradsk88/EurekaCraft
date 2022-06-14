c3fb3eb4d1aee634db05c14fba205d6c7b0f7cc3
Brad Johnson <bradsk88@gmail.com>
2022-06-13 20:11:19 -0600

    Blueprints in all villages work now

--- src/main/java/ca/bradj/eurekacraft/core/events/LootEvents.java
+++ src/main/java/ca/bradj/eurekacraft/core/events/LootEvents.java
@@ -8,8 +8,6 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-
 @Mod.EventBusSubscriber(modid = EurekaCraft.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class LootEvents {
 
@@ -17,22 +15,16 @@ public class LootEvents {
     public static void registerModifierSerializers(
             final RegistryEvent.Register<GlobalLootModifierSerializer<?>> event
     ) {
-
-        ArrayList<String> files = new ArrayList<>();
-        files.add("blueprint_from_village_plains_chest");
-//        files.add("blueprint_from_village_savanna_chest");
-//        files.add("blueprint_from_village_desert_chest");
-//        files.add("blueprint_from_village_snowy_chest");
-//        files.add("blueprint_from_village_cartographer");
-//        files.add("blueprint_from_village_taiga_chest");
-//        files.add("blueprint_from_village_toolsmith_chest");
-//        files.add("blueprint_from_village_weaponsmith_chest");
-
-        for (String f : files) {
-            event.getRegistry().registerAll(new BlueprintsAdditionModifier.Serializer().setRegistryName(
-                    new ResourceLocation(EurekaCraft.MODID, f)
-            ));
-        }
+        event.getRegistry().registerAll(
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_plains_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_savanna_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_desert_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_snowy_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_cartographer")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_taiga_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_toolsmith_chest")),
+                new BlueprintsAdditionModifier.Serializer().setRegistryName(new ResourceLocation(EurekaCraft.MODID, "blueprint_from_village_weaponsmith_chest"))
+        );
     }
 
 }
