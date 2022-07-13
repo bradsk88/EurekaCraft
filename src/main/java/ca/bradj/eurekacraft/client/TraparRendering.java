9abde2d0028d74d35a41fe73b8732cdfa13e1e86
Brad Johnson <bradsk88@gmail.com>
2022-07-12 22:29:32 -0600

    WIP: Render Non-entity trapar wave blocks
    Might be something like this
    https://forums.minecraftforge.net/topic/81895-problems-with-rendering-a-line-1152/

--- src/main/java/ca/bradj/eurekacraft/client/TraparRendering.java
+++ src/main/java/ca/bradj/eurekacraft/client/TraparRendering.java
@@ -4,7 +4,7 @@ import ca.bradj.eurekacraft.EurekaCraft;
 import ca.bradj.eurekacraft.core.init.TilesInit;
 import ca.bradj.eurekacraft.render.TraparWaveHandler;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -15,5 +15,10 @@ public class TraparRendering {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(TilesInit.TRAPAR_WAVE.get(), TraparWaveHandler::new);
+    }
+
+    @SubscribeEvent
+    public static void registerDataRenderers(RenderLevelLastEvent evt){
+        evt.getLevelRenderer().rend;
     }
 }
