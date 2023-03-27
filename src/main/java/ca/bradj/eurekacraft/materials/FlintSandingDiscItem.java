ea9972dc08ffe447834339f7a4cd483313f308bd
Brad Johnson <bradsk88@gmail.com>
2023-03-26 18:43:40 -0600

    feat(): Improve sanding item tooltips

--- src/main/java/ca/bradj/eurekacraft/materials/FlintSandingDiscItem.java
+++ src/main/java/ca/bradj/eurekacraft/materials/FlintSandingDiscItem.java
@@ -3,10 +3,18 @@ package ca.bradj.eurekacraft.materials;
 import ca.bradj.eurekacraft.core.init.ModItemGroup;
 import ca.bradj.eurekacraft.interfaces.SandingMachineSlotAware;
 import ca.bradj.eurekacraft.world.NoisyItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public class FlintSandingDiscItem extends Item implements NoisyCraftingItem, SandingMachineSlotAware {
@@ -18,6 +26,19 @@ public class FlintSandingDiscItem extends Item implements NoisyCraftingItem, San
     private static final Properties PROPS = new Properties().tab(ModItemGroup.EUREKACRAFT_GROUP).
             durability(3).
             setNoRepair();
+
+    @Override
+    public void appendHoverText(
+            ItemStack p_41421_,
+            @Nullable Level p_41422_,
+            List<Component> p_41423_,
+            TooltipFlag p_41424_
+    ) {
+        p_41423_.add(
+                new TranslatableComponent("item.eurekacraft.sanding_discs.subtitle").
+                        withStyle(ChatFormatting.GRAY)
+        );
+    }
 
     public FlintSandingDiscItem() {
         super(PROPS);
